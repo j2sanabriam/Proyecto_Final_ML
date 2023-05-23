683a28f6debc4e3ac54509a7833d0f83145dab48
j2sanabriam <jj.sanabria@uniandes.edu.co>
2023-05-22 23:34:45 -0500

    funciona

--- App/auxiliar.py
+++ App/auxiliar.py
@@ -2,22 +2,37 @@ import pandas as pd
 import streamlit as st
 import requests
 import joblib
+import pickle
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, FunctionTransformer
+from sklearn.compose import ColumnTransformer, make_column_selector
+import numpy as np
+from scipy.sparse import csr_matrix
 
-# from sklearn.externals import joblib
+
+def load_original_data():
+    data = pd.read_csv("./data/solicitudes_tc_dataset.csv", sep=";")
+    data2 = data.drop(['moroso'], axis=1)
+    return data2
 
 
-
-def read_file(flile):
-    df = pd.read_csv(flile, sep=";")
+def read_file(file):
+    df = pd.read_csv(file, sep=";")
     return df
 
 
 # @st.experimental_singleton()
 @st.cache_data
 def load_model():
-    path = './models/RandomForest.pkl'
+    path = './models/SVM.pkl'
+    # modelo = pickle.load(path)
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model
+    """
     try:
-        url = 'https://github.com/j2sanabriam/Proyecto_Final_ML/blob/main/models/RandomForest.pkl'
+        
+        url = 'https://github.com/j2sanabriam/Proyecto_Final_ML/blob/main/models/SVM.pkl'
         print("Downloading from GitHub")
         r = requests.get(url)
 
@@ -25,13 +40,27 @@ def load_model():
             file.write(r.content)
 
         print("Downloaded from GitHub")
+        
         return joblib.load(path)
     except:
         return joblib.load(path)
+    """
+
 
 @st.cache_data
 def load_pipeline():
+    """
+    path = './models/pipeline_2.pkl'
+    with open(path, 'rb') as f:
+        pipe = pickle.load(f)
+    return pipe
+    """
     path = './models/pipeline.pkl'
+    with open(path, 'rb') as f:
+        pipe = joblib.load(f)
+    return pipe
+
+    """
     try:
         url = 'https://github.com/j2sanabriam/Proyecto_Final_ML/blob/main/models/pipeline.pkl'
         print("Downloading from GitHub")
@@ -44,3 +73,8 @@ def load_pipeline():
         return joblib.load(path)
     except:
         return joblib.load(path)
+    """
+
+
+
+
